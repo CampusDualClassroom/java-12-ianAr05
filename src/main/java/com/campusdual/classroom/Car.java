193ce0fcd06e1953eb700d6edc397810c9bd64e1
Ian <ianagrafojo2@gmail.com>
2025-05-27 09:30:02 +0200

    12

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -2,4 +2,106 @@ package com.campusdual.classroom;
 
 public class Car {
 
+    public String brand;
+    public String model;
+    public static final int MAX_SPEED = 120;
+    public String fuel;
+    public int speedometer = 0;
+    public int tachometer = 0;
+    public String gear = "N";
+    public boolean reverse = false;
+    public int wheelsAngle = 0;
+
+    // Constructor con parámetros
+    public Car(String brand, String model, String fuel) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = fuel;
+    }
+
+    public Car() {
+        this.brand = "Unknown";
+        this.model = "Unknown";
+        this.fuel = "Gasoline";
+    }
+
+    public boolean isTachometerGreaterThanZero() {
+        return this.tachometer > 0;
+    }
+
+    public boolean isTachometerEqualToZero() {
+        return this.tachometer == 0;
+    }
+
+    public void start() {
+        if (this.tachometer == 0) {
+            this.tachometer = 1000;
+            System.out.println("Vehículo encendido");
+        } else {
+            System.out.println("El vehículo ya está encendido");
+        }
+    }
+
+    public void stop() {
+        if (this.speedometer == 0) {
+            this.tachometer = 0;
+            this.gear = "N";
+            this.reverse = false;
+            System.out.println("Vehículo apagado");
+        } else {
+            System.out.println("No se puede apagar el vehículo en movimiento");
+        }
+    }
+
+    public void accelerate() {
+        if (this.tachometer > 0 && this.speedometer < MAX_SPEED) {
+            this.speedometer += 5;
+            if (this.speedometer > MAX_SPEED) {
+                this.speedometer = MAX_SPEED;
+            }
+        }
+    }
+
+    public void brake() {
+        if (this.speedometer > 0) {
+            this.speedometer -= 5;
+            if (this.speedometer < 0) {
+                this.speedometer = 0;
+            }
+        }
+    }
+
+    public void turnAngleOfWheels(int angle) {
+        int newAngle = this.wheelsAngle + angle;
+        if (newAngle > 45) {
+            this.wheelsAngle = 45;
+        } else if (newAngle < -45) {
+            this.wheelsAngle = -45;
+        } else {
+            this.wheelsAngle = newAngle;
+        }
+    }
+
+    public void setReverse(boolean reverse) {
+        if (this.speedometer == 0 && reverse) {
+            this.reverse = true;
+            this.gear = "R";
+        } else if (!reverse) {
+            this.reverse = false;
+            this.gear = "N";
+        } else {
+
+        }
+    }
+
+    public void showDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel: " + fuel);
+        System.out.println("Speedometer: " + speedometer);
+        System.out.println("Tachometer: " + tachometer);
+        System.out.println("Gear: " + gear);
+        System.out.println("Reverse: " + reverse);
+        System.out.println("Wheels angle: " + wheelsAngle);
+    }
 }
--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -1,8 +1,77 @@
 package com.campusdual.classroom;
 
 public class Exercise12 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-    }
+                Car myCar = new Car("Toyota", "Yaris", "Gasolina");
 
+                System.out.println("¿Tacómetro está a cero? " + myCar.isTachometerEqualToZero());
+
+                System.out.println("Encendiendo el coche...");
+                myCar.start();
+
+                System.out.println("Intentando encender de nuevo...");
+                myCar.start();
+
+                System.out.println("Apagando el coche...");
+                myCar.stop();
+
+                System.out.println("Volvemos a encender para continuar...");
+                myCar.start();
+
+                System.out.println("Acelerando...");
+                myCar.accelerate();
+                System.out.println("Velocidad: " + myCar.speedometer);
+
+                System.out.println("Intentando apagar el coche en movimiento...");
+                myCar.stop();
+
+                System.out.println("Acelerando al máximo...");
+                for (int i = 0; i < 30; i++) {
+                        myCar.accelerate();
+                }
+                System.out.println("Velocidad final: " + myCar.speedometer);
+
+                System.out.println("Frenando una vez...");
+                myCar.brake();
+                System.out.println("Velocidad actual: " + myCar.speedometer);
+
+                System.out.println("Frenando muchas veces...");
+                for (int i = 0; i < 10; i++) {
+                        myCar.brake();
+                }
+                System.out.println("Velocidad actual: " + myCar.speedometer);
+
+                System.out.println("Girando 20 grados...");
+                myCar.turnAngleOfWheels(20);
+                System.out.println("Ángulo del volante: " + myCar.wheelsAngle);
+
+                System.out.println("Girando 200 grados...");
+                myCar.turnAngleOfWheels(200);
+                System.out.println("Ángulo del volante: " + myCar.wheelsAngle);
+
+                System.out.println("Girando -200 grados...");
+                myCar.turnAngleOfWheels(-200);
+                System.out.println("Ángulo del volante: " + myCar.wheelsAngle);
+
+                System.out.println("Frenando por completo para activar marcha atrás...");
+                for (int i = 0; i < 10; i++) {
+                        myCar.brake();
+                }
+                myCar.setReverse(true);
+                System.out.println("Marcha actual: " + myCar.gear);
+
+                System.out.println("Quitando marcha atrás y acelerando hacia adelante...");
+                myCar.setReverse(false);
+                myCar.accelerate();
+                myCar.accelerate();
+                System.out.println("Velocidad: " + myCar.speedometer);
+
+                System.out.println("Intentando poner marcha atrás en movimiento...");
+                myCar.setReverse(true);
+                System.out.println("Marcha actual: " + myCar.gear);
+
+                System.out.println("Mostrando detalles del coche:");
+                myCar.showDetails();
+        }
 }
